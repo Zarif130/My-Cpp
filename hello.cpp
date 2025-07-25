8639fc8d647da2cedbe2d53bd696f634c7b38136
Zarif130 <jarifh130@gmail.com>
2025-07-26 03:34:00 +0600

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello!";
+    cout<<endl;//akta enter diye dilam.
+    cout<<"\n";//akta enter diye dilam.
+    cout<<"learn cpp";
+}
