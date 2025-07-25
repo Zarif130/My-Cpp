2c2c6b0f2d38f6119d6eee4b0184b568d04243b3
Zarif130 <jarifh130@gmail.com>
2025-07-26 03:46:03 +0600

    Added.

---
+++ Basic/updation_of_variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+     x = 9;
+    cout<<x<<endl;
+     x +=7;
+    cout<<x<<endl;
+     x = x+4;
+    cout<<x<<endl;
+}
---
+++ Basic/variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int x = 5;
+ cout<<"so x="; 
+ cout<<x;  cout<<endl;
+ int y=6;
+ cout<<x+y;
+}
