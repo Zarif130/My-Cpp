1c47e3c31ecc5c96fe9573e81bdd803293f0f5d0
Zarif130 <jarifh130@gmail.com>
2025-09-01 18:30:08 +0600

    Added.

---
+++ Conditionals/a-question.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+if (a%5==0||a%3==0||a%15!=0)
+    {
+        cout <<"yes";
+
+    }
+    else
+    cout <<"no ";
+}
---
+++ Conditionals/absolute_value.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int a,x,t;
+    cin>>a;
+    int y= abs(a);
+    cout <<y;
+}
